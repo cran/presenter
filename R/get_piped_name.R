7cc1e0dbabb7a502241e437fe8be34c0376bf014
Harrison Tietze <Harrison4192@gmail.com>
2021-11-18 05:20:05 +0000

    version 0.1.1

--- R/get_piped_name.R
+++ R/get_piped_name.R
@@ -1,59 +1,152 @@
 
-
-#' get lhs
-#'
-#' @keywords internal
-#'
-get_lhs <- function(){
-  calls <- sys.calls()
-
-  call_firsts <- lapply(calls,`[[`,1)
-  pipe_calls <- vapply(call_firsts,identical,logical(1),quote(`%>%`))
-  if(all(!pipe_calls)){
-    NULL
-  } else {
-    pipe_calls <- which(pipe_calls)
-    pipe_calls <- pipe_calls[length(pipe_calls)]
-    calls[[c(pipe_calls,2)]]
-  }
-}
-
 #' get piped name
 #'
 #'this function captures the name of an object piped into a function, and returns as a string. Powers the automatic naming found in presenter.
 #'
-#'citation: \url{https://michaelbarrowman.co.uk/post/getting-a-variable-name-in-a-pipeline/}
 #'
 #'
-#' @param objName an object
-#' @param default_name default name if object name is not able to be accessed in case of a long pipe
+#'
+#' @param object an object
+#' @param default_name string Attempts to return this string if an error occurs.
 #'
 #' @return string
 #' @export
 #'
 #' @examples
 #'
-#' ## works if the object is piped or given as an argument
+#' #necessary to specify this option when using get_piped_name in knitr
+#' options(rlang_trace_top_env = rlang::current_env())
+#'
+#'
+#' ### works if the object is piped or given as an argument
 #' iris %>%
-#' get_piped_name
+#' get_piped_name()
 #'
 #' get_piped_name(iris)
 #'
-#' ## does not work for multistep pipes. instead the 'default_name' is printed
+#' ### can even extract name from multistep pipes
 #' iris %>%
 #' dplyr::select(1:3) %>%
-#' get_piped_name
-get_piped_name <- function(objName, default_name = "Title") {
-  z <- get_lhs()
+#' get_piped_name()
+#'
+#' ### can be placed inside other functions to capture the name and save it
+#'
+#' find_name <- function(x){
+#'  get_piped_name() -> new_name
+#'
+#'  new_name
+#' }
+#'
+#' iris %>%
+#' dplyr:select(1:3) %>%
+#' find_name()
+#'
+get_piped_name <- function(object, default_name = "Table") {
 
-  if (is.null(z)) {
-    z <- rlang::enexpr(objName)
+  rlang::trace_back() -> f_trace
+
+  f_trace$calls -> f_calls
+
+  length(f_calls) -> f_len
+
+## if the option isn't specified, this workaround can bypass all the calls from knitr
+ if(f_len > 7){
+   f_calls[(f_len-2):f_len] -> f_calls
+ }
+
+  f_calls %>%
+    purrr::pluck(1) %>%
+    as.character() %>%
+    purrr::pluck(2) %>%
+    stringr::str_split(pattern = stringr::boundary("word")) %>%
+    purrr::pluck(1, 1) -> the_call
+
+  if(is.null(the_call)){
+
+    the_call <- default_name
   }
 
-  if (rlang::is_symbol(z)) {
-    z <- rlang::as_name(z)
-  } else{
-    z <- default_name
-  }
-  z
+  the_call
+
 }
+
+## debug function in vignettes
+
+#' new_fun <- function(x){
+#'
+#'   get_piped_name()
+#' }
+#'
+#'
+#' #' new fun pipe
+#' #'
+#' #' @param x
+#' #'
+#' #' @export
+#' new_fun_pipe <- function(x){
+#'
+#'  new_pipe() -> cl
+#'
+#' cl
+#'
+#' }
+#'
+#' #' new pipe
+#' #'
+#' #' @param object
+#' #'
+#' #' @export
+#' new_pipe <- function(object){
+#'
+#'
+#'   rlang::trace_back() -> tr
+#'   tr$calls
+#' }
+#
+# ---
+#   title: "pipedname"
+# output: rmarkdown::html_vignette
+# vignette: >
+#   %\VignetteIndexEntry{pipedname}
+# %\VignetteEngine{knitr::rmarkdown}
+# %\VignetteEncoding{UTF-8}
+# ---
+#
+#   ```{r, include = FALSE}
+# knitr::opts_chunk$set(
+#   collapse = TRUE,
+#   comment = "#>"
+# )
+# ```
+#
+# ```{r setup}
+# library(presenter)
+# library(dplyr)
+# library(purrr)
+# ```
+#
+# ```{r}
+# iris %>%
+#   mutate(Species = Species) %>%
+#   new_fun_pipe() %>%
+#   pluck(1)
+# ```
+#
+# ```{r}
+# iris %>%
+#   mutate(Species = Species) %>%
+#   get_piped_name()
+# ```
+#
+# ```{r}
+# iris %>%
+#   mutate(Species = Species) %>%
+#   new_fun() %>%
+#   new_fun()
+# ```
+#
+# ```{r}
+# iris %>%
+#   mutate(Species = Species) %>%
+#   new_fun_pipe()
+# ```
